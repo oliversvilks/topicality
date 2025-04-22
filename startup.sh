f51e761682d8e6972c0d5334dec2af151732a98e
Olivers Vilks <oliversv@ergo.lv>
2025-04-22 22:11:08 +0300

    startup fix

--- startup.sh
+++ startup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
+set -e
 
-set -e  # Exit on error
-
-pip install -r /home/site/wwwroot/requirements.txt
-
-gunicorn --bind=0.0.0.0 --timeout 600 --chdir src api:app --workers 4 --worker-class uvicorn.workers.UvicornWorker
+gunicorn src.api:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0
