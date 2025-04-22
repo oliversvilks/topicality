27dc0a028ac411deb11a7022e92ebe961df11a9d
Olivers Vilks <oliversv@ergo.lv>
2025-04-22 20:07:22 +0300

    startup change

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@ set -e  # Exit on error
 
 pip install -r /home/site/wwwroot/requirements.txt
 
-gunicorn src.api:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8080
+gunicorn --bind=0.0.0.0 --timeout 600 --chdir src api:app --workers 4 --worker-class uvicorn.workers.UvicornWorker
