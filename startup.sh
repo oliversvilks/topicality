a8529ce0b2efe13863de4384239015309f57ad41
Olivers Vilks <oliversv@ergo.lv>
2025-04-22 17:07:19 +0300

    dependencies install

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install dependencies
-#pip install -r /home/site/wwwroot/requirements.txt
+pip install -r /home/site/wwwroot/requirements.txt
 
 # Start the application using Gunicorn with Uvicorn workers
 gunicorn src.api:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8080
