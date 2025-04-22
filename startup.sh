ff6554be421cc38cf0e9391bd7068f960a8091c3
Olivers Vilks <oliversv@ergo.lv>
2025-04-22 17:21:20 +0300

    reqs update

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Install dependencies
+set -e  # Exit on error
+
 pip install -r /home/site/wwwroot/requirements.txt
 
-# Start the application using Gunicorn with Uvicorn workers
 gunicorn src.api:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8080
