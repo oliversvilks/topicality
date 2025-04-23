9985ee9b8e5742760561d3073b4b77d703860971
Olivers Vilks <oliversv@ergo.lv>
2025-04-23 09:12:15 +0300

    startup changes

--- startup.sh
+++ startup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
 
-gunicorn src.api:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0
+gunicorn src.api:app \
+  --workers 4 \
+  --worker-class uvicorn.workers.UvicornWorker \
+  --bind 0.0.0.0:${PORT}
